6ec85773e95ef39cc118588c75c6f0e356ad2ee1
Deepanker Tiwari <deepamamazing@gmail.com>
2025-05-11 00:42:19 +0530

    Namaste React Series-1.2

---
+++ App.js
@@ -0,0 +1,10 @@
+const heading = React.createElement("h1",
+    { id: "heading" },
+    "Hellow World From React!!")
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(heading)
+
+console.log(heading);       //Object
+
