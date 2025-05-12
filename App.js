04f1a01d8e8f2c6b03171e27d7da3efb39ce20b0
Deepanker Tiwari <deepamamazing@gmail.com>
2025-05-12 21:56:26 +0530

    Namaste React Series-1.3

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-const heading = React.createElement("h1",
-    { id: "heading" },
-    "Hellow World From React!!")
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement("div",
+        { id: "child" },[
+        React.createElement("h1",{},"I'm an h1 tag"),
+        React.createElement("h2",{},"I'm an h2 tag"),
+    ])
+)
+
+
+// const heading = React.createElement("h1",
+//     { id: "heading", xyz: "abc" },
+//     "Hellow World From React!!")
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(heading)
+root.render(parent)
+// root.render(heading)
 
-console.log(heading);       //Object
+// console.log(heading);       //Object
 
