d6dc0e51e91b6c35787d9415a4c8924894e466d1
Deepanker Tiwari <deepamamazing@gmail.com>
2025-05-13 23:38:17 +0530

    Namaster React-2.1

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
+//React Element is an object and not and HTML
+
 const parent = React.createElement(
     "div",
     { id: "parent" },
     React.createElement("div",
         { id: "child" },[
+        React.createElement("h1",{},"I'm an h1 tag"),
+        React.createElement("h2",{},"I'm an h2 tag"),
+    ]),
+        React.createElement("div",
+        { id: "child2" },[
         React.createElement("h1",{},"I'm an h1 tag"),
         React.createElement("h2",{},"I'm an h2 tag"),
     ])
