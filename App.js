79d5c628725d4beb262f2f045a0ecc2bead09b17
Deepanker Tiwari <deepamamazing@gmail.com>
2025-05-16 23:26:33 +0530

    Namaster React-3.1

--- App.js
+++ App.js
@@ -3,30 +3,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    React.createElement("div",
-        { id: "child" },[
-        React.createElement("h1",{key: "unique-key-h1"},"I'm an h1 tag"),
-        React.createElement("h2",{key: "unique-key2-h1"},"I'm an h2 tag"),
-    ]),
-        React.createElement("div",
-        { id: "child2" },[
-        React.createElement("h1",{key: "unique-key-h2"},"I'm an h1 tag"),
-        React.createElement("h2",{key: "unique-key2-h2"},"I'm an h2 tag"),
-    ])
-)
+// const parent = React.createElement(
+//     "div",
+//     { id: "parent" },
+//     React.createElement("div",
+//         { id: "child" },[
+//         React.createElement("h1",{key: "unique-key-h1"},"I'm an h1 tag"),
+//         React.createElement("h2",{key: "unique-key2-h1"},"I'm an h2 tag"),
+//     ]),
+//         React.createElement("div",
+//         { id: "child2" },[
+//         React.createElement("h1",{key: "unique-key-h2"},"I'm an h1 tag"),
+//         React.createElement("h2",{key: "unique-key2-h2"},"I'm an h2 tag"),
+//     ])
+// )
 
 
 // const heading = React.createElement("h1",
 //     { id: "heading", xyz: "abc" },
 //     "Hellow World From React!!")
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+// const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(parent)
+// root.render(parent)
 // root.render(heading)
 
 // console.log(heading);       //Object
 
+
+
+//JSX - HTML-like or XML-like syntax
+
+const jsonHeading=<h1 id="headings">Namaste React using JSx</h1>
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(jsonHeading)
