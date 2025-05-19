1a99a6dbb7392797b1203be87bd6bae659abd3c6
Deepanker Tiwari <deepamamazing@gmail.com>
2025-05-19 23:27:27 +0530

    Changes in Folder and Namaste react 4.2 continue..

--- Food Delivery App/App.js
+++ Food Delivery App/App.js
@@ -1,4 +1,5 @@
 //React Element is an object and not and HTML
+//Note: There's a term known as config driven ui
 
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -28,18 +29,87 @@ const styleCard = {
   backgroundColor: "#f0f0f0",
 };
 
+const RestaurentCard2 = ({ resName, cuisine }) => {
+  console.log("This is destructing on the fly");
+};
+
+const resObj = {
+  id: "1003414",
+  name: "Pizza Hut",
+  cloudinaryImageId:
+    "RX_THUMBNAIL/IMAGES/VENDOR/2025/4/9/6513abee-f904-4a0e-9b49-90d1cc29551c_1003414.jpg",
+  locality: "Chhindwara",
+  areaName: "Parasia Road",
+  costForTwo: "₹350 for two",
+  cuisines: ["Pizzas"],
+  avgRating: 4.1,
+  parentId: "721",
+  avgRatingString: "4.1",
+  totalRatingsString: "86",
+  sla: {
+    deliveryTime: 48,
+    lastMileTravel: 13.1,
+    serviceability: "SERVICEABLE",
+    slaString: "45-50 mins",
+    lastMileTravelString: "13.1 km",
+    iconType: "ICON_TYPE_EMPTY",
+  },
+  availability: {
+    nextCloseTime: "2025-05-19 23:00:00",
+    opened: true,
+  },
+  badges: {},
+  isOpen: true,
+  type: "F",
+  badgesV2: {
+    entityBadges: {
+      imageBased: {},
+      textBased: {},
+      textExtendedBadges: {},
+    },
+  },
+  aggregatedDiscountInfoV3: {
+    header: "ITEMS",
+    subHeader: "AT ₹59",
+  },
+  orderabilityCommunication: {
+    title: {},
+    subTitle: {},
+    message: {},
+    customIcon: {},
+  },
+  differentiatedUi: {
+    displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    differentiatedUiMediaDetails: {
+      mediaType: "ADS_MEDIA_ENUM_IMAGE",
+      lottie: {},
+      video: {},
+    },
+  },
+  reviewsSummary: {},
+  displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+  isNewlyOnboarded: true,
+  restaurantOfferPresentationInfo: {},
+  externalRatings: {
+    aggregatedRating: {
+      rating: "--",
+    },
+  },
+  ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+};
+
+
 const RestaurentCard = (props) => {
+  const { resData } = props;
   console.log(props);
   return (
-    
     <div className="res-card" style={styleCard}>
       <img
         className="res-logo"
         alt="res-food"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Chole_Bhature_At_Local_Street.jpg/330px-Chole_Bhature_At_Local_Street.jpg"
       />
-      <h3>Haldiram</h3>
-      <h4>Chole Bhature, India</h4>
+      <h3>{resData.name}</h3>
       <h4>4.5 stars</h4>
       <h4>30 minutes</h4>
     </div>
@@ -51,20 +121,7 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurentCard resName="Haldiram" cuisine="Chole Bhature"/>
-        <RestaurentCard resName="KFC" cuisine="Chicken Strips"/>
-        <RestaurentCard />
-        <RestaurentCard />
-        <RestaurentCard />
-        <RestaurentCard />
-        <RestaurentCard />
-        <RestaurentCard />
-        <RestaurentCard />
-        <RestaurentCard />
-        <RestaurentCard />
-        <RestaurentCard />
-        <RestaurentCard />
-        <RestaurentCard />
+        <RestaurentCard resData={resObj} />
       </div>
     </div>
   );
