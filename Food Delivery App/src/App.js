a86b6409040f46e12aae72961c55e93d3c2cbd85
Deepanker Tiwari <deepamamazing@gmail.com>
2025-05-21 23:57:32 +0530

    Namaste react 5

--- Food Delivery App/src/App.js
+++ Food Delivery App/src/App.js
@@ -4,26 +4,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Header = () => {
-  return (
-    <div className="header">
-      <div>
-        <img
-          className="logo"
-          src="https://mms.businesswire.com/media/20201011005092/en/829342/4/Food-e_logo_black.jpg?download=1"
-        />
-      </div>
-      <div className="nav-items">
-        <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
+
 
 const styleCard = {
   backgroundColor: "#f0f0f0",
---
+++ Food Delivery App/src/components/Header.js
@@ -0,0 +1,22 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <img
+          className="logo"
+          src="https://mms.businesswire.com/media/20201011005092/en/829342/4/Food-e_logo_black.jpg?download=1"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
