29eaf6d28757e70cb9526730cc75ee28939bb296
Deepanker Tiwari <deepamamazing@gmail.com>
2025-06-20 23:34:39 +0530

    Namaste React 10.4

--- Food Delivery App/src/components/ItemList.js
+++ Food Delivery App/src/components/ItemList.js
@@ -1,13 +1,22 @@
+import { CDN_URL } from "../utils/constants"
+
 const ItemList = ({ items }) => {
     return (
         <div>
             {items.map(item => (
-                <div key={item.card.info.id} className="p-2 m-2 border-gray-200 border-b-2 text-left">
-                    <div>
-                        <span>{item.card.info.name}</span>
-                        <span>- ₹ {item.card.info.defaultPrice}</span>
+                <div key={item.card.info.id} className="p-2 m-2 border-gray-200 border-b-2 text-left flex">
+                    <div className="w-9/12">
+                        <div className="py-2">
+                            <span>{item.card.info.name}</span>
+                            <span>- ₹ {item.card.info.price
+                                ? item.card.info.price / 100
+                                : item.card.info.defaultPrice}</span>
+                        </div>
+                        <p className="text-xs">{item.card.info.description}</p>
                     </div>
-                    <p className="text-xs">{item.card.info.description}</p>
+                    <div className="w-3/12">
+                        <img src={CDN_URL + item.card.info.imageId}  className="w-full" />
+                    </div>
                 </div>
             ))}
         </div>
