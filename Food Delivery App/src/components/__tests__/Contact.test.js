5148adfa13741478efa0938150d8c945cdd716be
Deepanker Tiwari <deepamamazing@gmail.com>
2025-06-25 10:08:07 +0530

    Namaste React 12~ unit testing..

--- Food Delivery App/src/components/__tests__/Contact.test.js
+++ Food Delivery App/src/components/__tests__/Contact.test.js
@@ -2,39 +2,55 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
-test("Should load contact us component", () => {
-    render(<Contact />)
 
-    const heading = screen.getByRole("heading");
+describe("Contact Us Page Test Case", () => {
+    test("Should load contact us component", () => {
+        render(<Contact />)
 
-    //Assertion
-    expect(heading).toBeInTheDocument();
+        const heading = screen.getByRole("heading");
+
+        //Assertion
+        expect(heading).toBeInTheDocument();
+    })
+
+
+    //test or it both are same
+    it("Should load button inside Contact component", () => {
+        render(<Contact />)
+
+        // const button = screen.getByRole("button");
+        const button = screen.getByText("Submit");
+
+        //Assertion
+        expect(button).toBeInTheDocument();
+    })
+
+
+
+    test("Should load input name inside Contact component", () => {
+        render(<Contact />)
+
+        const button = screen.getByPlaceholderText("name");
+
+        //Assertion
+        expect(button).toBeInTheDocument();
+    })
+
+    test("Should load 2 input boxes on the Contact Component", () => {
+        render(<Contact />)
+
+        //Querying
+        const inputBoxes = screen.getAllByRole("textbox");
+
+        // console.log(inputBoxes.length);
+
+
+        //Assertion
+
+        // expect(inputBoxes.length).toBe(2);
+        // expect(inputBoxes.length).not.toBe(3);
+        expect(inputBoxes.length).toBeTruthy();
+
+    })
 })
 
-
-test("Should load button inside Contact component", () => {
-    render(<Contact />)
-
-    // const button = screen.getByRole("button");
-    const button = screen.getByText("Submit");
-
-    //Assertion
-    expect(button).toBeInTheDocument();
-})
-
-
-
-test("Should load input name inside Contact component", () => {
-    render(<Contact />)
-
-    const button = screen.getByPlaceholderText("name");
-
-    //Assertion
-    expect(button).toBeInTheDocument();
-})
-
-test("Should load 2 inout boxes on the Contact Component",()=>{
-    render(<Contact/>)
-
-    const inputBoxes=screen.getByA
-})
