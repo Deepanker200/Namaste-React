51b2132f60bbbff09224268695b289ddd22f8ac6
Deepanker Tiwari <deepamamazing@gmail.com>
2025-06-26 19:49:15 +0530

    Namaste React 14 ~ Created Project and Install Tailwind

---
+++ netflix-gpt/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <div className='App'>
+        <h1 className='text-3xl bg-purple-400 font-bold text-white'>Let's Build Netflix GPT</h1>
+      </div>     
+  )
+}
+
+export default App
